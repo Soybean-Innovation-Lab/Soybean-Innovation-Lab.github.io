8f074b315c0effb13af76f80799ddf09931e6db3
Gabe Cohen <gcc3.14@gmail.com>
2020-12-21 09:30:53 -0600

    Add the map

---
+++ sil-interactive-soybean-map/precache-manifest.defa0071c48d1a2e1eb924b062dc2042.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e671d81d5877abe85a772c8626613d9",
+    "url": "/sil-interactive-soybean-map/index.html"
+  },
+  {
+    "revision": "f1b88942e7b80326c8f2",
+    "url": "/sil-interactive-soybean-map/static/css/2.b4a16772.chunk.css"
+  },
+  {
+    "revision": "dcb4f262488bc96bbc19",
+    "url": "/sil-interactive-soybean-map/static/css/main.1c96ea3f.chunk.css"
+  },
+  {
+    "revision": "f1b88942e7b80326c8f2",
+    "url": "/sil-interactive-soybean-map/static/js/2.4c71e82b.chunk.js"
+  },
+  {
+    "revision": "14dc907096ff79e6b7be55307d0260dd",
+    "url": "/sil-interactive-soybean-map/static/js/2.4c71e82b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcb4f262488bc96bbc19",
+    "url": "/sil-interactive-soybean-map/static/js/main.9341fcdc.chunk.js"
+  },
+  {
+    "revision": "7b4cbd267058286c8e5b",
+    "url": "/sil-interactive-soybean-map/static/js/runtime-main.ef8feb9c.js"
+  },
+  {
+    "revision": "e347236a1b0c7436ae76e1e0665de527",
+    "url": "/sil-interactive-soybean-map/static/media/agronomy_blue.e347236a.png"
+  },
+  {
+    "revision": "ad367168f9e5a860f851e12773b24165",
+    "url": "/sil-interactive-soybean-map/static/media/variety_blue.ad367168.png"
+  }
+]);

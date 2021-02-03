3d1a88eddefd92d9b37c7b81be18bf1180c7ed1a
Gabe Cohen <gcc3.14@gmail.com>
2021-02-03 15:35:38 -0600

    eric's fix

---
+++ sil-interactive-soybean-map-dev/precache-manifest.4715d936055ee1a24984252277e6107a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "744a700960c147b3c9658027a4f81b01",
+    "url": "/sil-interactive-soybean-map-dev/index.html"
+  },
+  {
+    "revision": "895c0ee7cb31e5e4f36d",
+    "url": "/sil-interactive-soybean-map-dev/static/css/2.b4a16772.chunk.css"
+  },
+  {
+    "revision": "f398b280e1eda981312d",
+    "url": "/sil-interactive-soybean-map-dev/static/css/main.1c96ea3f.chunk.css"
+  },
+  {
+    "revision": "895c0ee7cb31e5e4f36d",
+    "url": "/sil-interactive-soybean-map-dev/static/js/2.7a0f9401.chunk.js"
+  },
+  {
+    "revision": "14dc907096ff79e6b7be55307d0260dd",
+    "url": "/sil-interactive-soybean-map-dev/static/js/2.7a0f9401.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f398b280e1eda981312d",
+    "url": "/sil-interactive-soybean-map-dev/static/js/main.e9e6149d.chunk.js"
+  },
+  {
+    "revision": "37458be46d1517d3f30e",
+    "url": "/sil-interactive-soybean-map-dev/static/js/runtime-main.538d812c.js"
+  },
+  {
+    "revision": "e347236a1b0c7436ae76e1e0665de527",
+    "url": "/sil-interactive-soybean-map-dev/static/media/agronomy_blue.e347236a.png"
+  },
+  {
+    "revision": "b9fd4f404b21db862ea99b7ecd26a376",
+    "url": "/sil-interactive-soybean-map-dev/static/media/disease.b9fd4f40.png"
+  },
+  {
+    "revision": "51b19520aee0b8efec499aff84ddd360",
+    "url": "/sil-interactive-soybean-map-dev/static/media/disease_blue.51b19520.png"
+  },
+  {
+    "revision": "ad367168f9e5a860f851e12773b24165",
+    "url": "/sil-interactive-soybean-map-dev/static/media/variety_blue.ad367168.png"
+  }
+]);
---
+++ sil-interactive-soybean-map-dev/service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/sil-interactive-soybean-map-dev/precache-manifest.4715d936055ee1a24984252277e6107a.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sil-interactive-soybean-map-dev/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
